dbc7ad0eb466f8ea09f26723c315b5bf52f2eacc
DevGisoun <devgisoun@gmail.com>
2023-01-03 12:51:14 +0900

    Chapter #4

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,6 +4,8 @@ import 'package:instagram_clone/src/components/img_data.dart';
 import 'package:instagram_clone/src/controller/bottom_nav_controller.dart';
 import 'package:instagram_clone/src/pages/home.dart';
 
+import 'pages/search.dart';
+
 class App extends GetView<BottomNavController> {
   const App({Key? key}) : super(key: key);
 
@@ -17,9 +19,7 @@ class App extends GetView<BottomNavController> {
             index: controller.pageIndex.value,
             children: [
               const Home(),
-              Container(
-                child: Center(child: Text('SEARCH')),
-              ),
+              const Search(),
               Container(
                 child: Center(child: Text('UPLOAD')),
               ),
