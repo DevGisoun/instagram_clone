53d9eba3725e93cfc7c8c9f67eba39a80d8d4294
DevGisoun <devgisoun@gmail.com>
2023-01-03 15:19:57 +0900

    Chapter #5

--- lib/src/pages/search.dart
+++ lib/src/pages/search.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:instagram_clone/src/pages/search/search_forcus.dart';
 import 'package:quiver/iterables.dart';
 
 class Search extends StatefulWidget {
@@ -31,31 +32,41 @@ class _SearchState extends State<Search> {
       groupBox[gi].add(size);
       groupIndex[gi] += size;
     }
-    print(groupBox);
   }
 
   Widget _appbar() {
     return Row(
       children: [
         Expanded(
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-            margin: const EdgeInsets.only(left: 15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(6),
-              color: const Color(0xffefefef),
-            ),
-            child: Row(
-              children: const [
-                Icon(Icons.search),
-                Text(
-                  '검색',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Color(0xff838383),
-                  ),
+          child: GestureDetector(
+            onTap: () {
+              // Get.to(SearchForcus());
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SearchForcus(),
                 ),
-              ],
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+              margin: const EdgeInsets.only(left: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                color: const Color(0xffefefef),
+              ),
+              child: Row(
+                children: const [
+                  Icon(Icons.search),
+                  Text(
+                    '검색',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xff838383),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
